2f39e3800448e621ecf95899c30c765d18f42262
Josh Wulf <josh@magikcraft.io>
2020-02-16 01:49:55 +1000

    JSON stringify job

--- workers/workers.js
+++ workers/workers.js
@@ -2,7 +2,7 @@ zbc.createWorkflowInstance("get-some", {});
 module.exports = {
   tasks: {
     "get-some": (job, complete) => {
-      core.info(job);
+      core.info(JSON.stringify(job));
       complete.success();
     }
   }
