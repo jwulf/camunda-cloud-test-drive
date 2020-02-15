33c0d642743fea30fd334d35f6f12ffebc60ec3a
Josh Wulf <josh@magikcraft.io>
2020-02-16 02:31:47 +1000

    Update log

--- workers/workers.js
+++ workers/workers.js
@@ -2,7 +2,8 @@ zbc.createWorkflowInstance("get-some", {});
 module.exports = {
   tasks: {
     "get-some": (job, complete) => {
-      core.info(JSON.stringify(job));
+      core.info(`Got a job from Camunda Cloud:`);
+      core.info(JSON.stringify(job, null, 2));
       complete.success();
     }
   }
